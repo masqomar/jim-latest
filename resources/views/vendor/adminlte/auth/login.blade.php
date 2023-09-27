7c314576b96d608f5ba7a038bc482c632921198a
unknown <qomaruddin5@gmail.com>
2023-09-27 15:21:39 +0700

    login

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -1,100 +1,109 @@
-@extends('adminlte::auth.auth-page', ['auth_type' => 'login'])
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-@section('adminlte_css_pre')
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/fontawesome.min.css') }}">
+    <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-@stop
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="card">
+    <div class="card">
+    <div class="card-header text-center bg-navy" style="background: url('polkadot.png') right;opacity: 0.9;position: cover;background-size: 60%;100%;background-repeat: no-repeat">
+        <img src="logo.png" alt="" style="width: 100px"><br>
+        <label style="font-size: 18px">KOPERASI KARYAWAN <br>
+            JAMAAH INSAN MULIA</label>
+    </div>
+    <div class="card-body login-card-body">
+        <p class="login-box-msg">Silahkan masuk untuk memulai sesi Anda</p>
 
-@php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
-@php( $register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register') )
-@php( $password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset') )
+        @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+        @endif
 
-@if (config('adminlte.use_route_url', false))
-    @php( $login_url = $login_url ? route($login_url) : '' )
-    @php( $register_url = $register_url ? route($register_url) : '' )
-    @php( $password_reset_url = $password_reset_url ? route($password_reset_url) : '' )
-@else
-    @php( $login_url = $login_url ? url($login_url) : '' )
-    @php( $register_url = $register_url ? url($register_url) : '' )
-    @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
-@endif
+        <form action="{{ $login_url }}" method="post">
+            @csrf
 
-@section('auth_header', __('adminlte::adminlte.login_message'))
-
-@section('auth_body')
-    <form action="{{ $login_url }}" method="post">
-        @csrf
-
-        {{-- Email field --}}
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
-
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+            <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="{{ __('Email') }}" required autofocus>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
                 </div>
+                @error('email')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
             </div>
 
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-
-        {{-- Password field --}}
-        <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
-
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('Password') }}" id="password" required>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
                 </div>
+                @error('password')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
             </div>
 
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-
-        {{-- Login field --}}
-        <div class="row mb-3">
-            <div class="col-6">
-                <div class="icheck-primary" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
+            <div class="row">
+                <div class="col-6">
+                    <div class="icheck-primary">
                     <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                    <label for="remember">
-                        {{ __('adminlte::adminlte.remember_me') }}
-                    </label>
+                        <label for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
                 </div>
-            </div>
-
-            <div class="col-6">
-                <div class="icheck-primary" title="{{ __('adminlte::adminlte.show_password') }}">
-                    <input type="checkbox" name="checkboxPassword" id="checkboxPassword">
-
-                    <label for="checkboxPassword">
-                        {{ __('adminlte::adminlte.show_password') }}
-                    </label>
+                <!-- /.col -->
+                <div class="col-6">
+                    <div class="icheck-primary">
+                        <input type="checkbox" id="checkboxPassword" name="checkboxPassword">
+                        <label for="checkboxPassword">
+                            {{ __('Show Password') }}
+                        </label>
+                    </div>
                 </div>
-            </div>           
-        </div>
-
-        <div class="row">
-            <div class="col-12">
-                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
-                    <span class="fas fa-sign-in-alt"></span>
-                    {{ __('adminlte::adminlte.sign_in') }}
-                </button>
+                <!-- /.col -->
             </div>
-        </div>
+            <br>
+            <button type="submit" class="btn btn-primary btn-block">{{ __('Login') }}</button>
+        </form>
+        <br>
+        @if (Route::has('password.request'))
+        <p class="mb-1">
+            <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
 
-    </form>
-@stop
-@section('js')
+        </p>
+        @endif
+    </div>
+    <!-- /.login-card-body -->
+    </div>
+</div>
+<!-- /.login-box -->
+
+<!-- Jquery -->
+<script src="{{ asset ('assets/js/lib/jquery-3.4.1.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
 <script>
     $(document).ready(function() {
         $('#checkboxPassword').on('change', function() {
@@ -102,23 +111,6 @@
         });
     });
 </script>
-@stop
-@section('auth_footer')
-    {{-- Password reset link --}}
-    @if($password_reset_url)
-        <p class="my-0">
-            <a href="{{ $password_reset_url }}">
-                {{ __('adminlte::adminlte.i_forgot_my_password') }}
-            </a>
-        </p>
-    @endif
-
-    {{-- Register link --}}
-    @if($register_url)
-        <p class="my-0">
-            <a href="{{ $register_url }}">
-                {{ __('adminlte::adminlte.register_a_new_membership') }}
-            </a>
-        </p>
-    @endif
-@stop
+@yield('scripts')
+</body>
+</html>
